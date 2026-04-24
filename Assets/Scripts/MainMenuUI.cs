ab821afaaab1fea84ab10bfe08bb5e1d81681a22
Ballsticles <132795890+Ballsticles@users.noreply.github.com>
2026-04-23 23:46:26 -0400

    Main Menu

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject titleMenu;
+    [SerializeField] private GameObject levelsMenu;
+    private bool loading = false;
+    public void OpenTitleMenu()
+    {
+        titleMenu.SetActive(true);
+        levelsMenu.SetActive(false);
+    }
+
+    public void OpenLevelsMenu()
+    {
+        titleMenu.SetActive(false);
+        levelsMenu.SetActive(true);
+    }
+    public void OpenOptionsMenu()
+    {
+        //nothing yet
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadSwingLevel()
+    {
+        if(loading) return;
+        SceneManager.LoadScene(1);
+        LevelLoading();
+    }
+    public void LoadJumpLevel()
+    {
+        if (loading) return;
+        LevelLoading();
+        SceneManager.LoadScene(2);
+    }
+    public void LoadTownLevel()
+    {
+        if (loading) return;
+        LevelLoading();
+        SceneManager.LoadScene(3);
+    }
+    public void LoadTestLevel()
+    {
+        if (loading) return;
+        LevelLoading();
+        SceneManager.LoadScene(4);
+    }
+
+    private void LevelLoading()
+    {
+        loading = true;
+    }
+}
